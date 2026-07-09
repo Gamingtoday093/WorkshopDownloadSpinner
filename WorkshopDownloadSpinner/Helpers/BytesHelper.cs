70837474d6c258e297d9b8e73a90f4fd4bc8ccae
Gamingtoday093 <Gamingtoday093@gmail.com>
2026-07-09 17:39:18 +0200

    Added Estimated Download Speed

---
+++ WorkshopDownloadSpinner/Helpers/BytesHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkshopDownloadSpinner.Helpers
+{
+    public static class BytesHelper
+    {
+        private static readonly string[] ByteUnits = ["B", "KB", "MB", "GB", "TB"];
+
+        public static string ToBytesString(this float bytes)
+        {
+            int unit = 0;
+            while (bytes > 1024f && unit < ByteUnits.Length - 1)
+            {
+                bytes /= 1024f;
+                unit++;
+            }
+
+            return $"{bytes:0.#} {ByteUnits[unit]}";
+        }
+    }
+}
--- WorkshopDownloadSpinner/Services/DownloadSpinnerService.cs
+++ WorkshopDownloadSpinner/Services/DownloadSpinnerService.cs
@@ -12,6 +12,7 @@ namespace WorkshopDownloadSpinner.Services
     {
         private const float DELAY_SECONDS = 1.5f;
         private const int PROGRESS_BAR_LENGTH = 16;
+        private const float UPDATE_ESTIMATE_RATE_SECONDS = 2.5f;
 
         public PublishedFileId_t WorkshopItem { get; private set; } = PublishedFileId_t.Invalid;
 
@@ -20,6 +21,10 @@ namespace WorkshopDownloadSpinner.Services
 
         private const char ProgressEmptyChar = ' ';
         private const char ProgressFilledChar = '#';
+
+        private float BytesPerSecond { get; set; }
+        private ulong LastDownloadedBytes { get; set; }
+        private float LastEstimate { get; set; } = Time.realtimeSinceStartup;
 
         public bool SpinnerRunning => UpdateSpinnerCoroutine != null;
         private Coroutine? UpdateSpinnerCoroutine { get; set; }
@@ -36,6 +41,10 @@ namespace WorkshopDownloadSpinner.Services
                 StopSpinner();
 
             WorkshopItem = workshopItem;
+
+            LastDownloadedBytes = 0;
+            LastEstimate = Time.realtimeSinceStartup;
+
             TempSteamworksWorkshop.getCachedDetails(WorkshopItem, out CachedUGCDetails cachedDetails);
             UpdateSpinnerCoroutine = StartCoroutine(UpdateSpinner($"Downloading \"{cachedDetails.GetTitle()}\" ({WorkshopItem})"));
         }
@@ -69,9 +78,37 @@ namespace WorkshopDownloadSpinner.Services
             return progressBar.ToString();
         }
 
+        private void UpdateEstimate(ulong bytesDownloaded, ulong bytesTotal)
+        {
+            if (bytesDownloaded == bytesTotal)
+            {
+                BytesPerSecond = 0f;
+                return;
+            }
+
+            if (BytesPerSecond != 0f && Time.realtimeSinceStartup - LastEstimate < UPDATE_ESTIMATE_RATE_SECONDS) return;
+            
+            BytesPerSecond = (float)((double)(bytesDownloaded - LastDownloadedBytes) / Time.realtimeSinceStartup - LastEstimate);
+            if (BytesPerSecond < 0f) BytesPerSecond = 0f;
+
+            LastDownloadedBytes = bytesDownloaded;
+            LastEstimate = Time.realtimeSinceStartup;
+        }
+
+        private string DownloadEstimate()
+        {
+            return $"{BytesPerSecond.ToBytesString()}/s";
+        }
+
         private IEnumerator UpdateSpinner(string downloadMessage)
         {
             yield return new WaitForEndOfFrame();
+
+            if (SteamGameServerUGC.GetItemDownloadInfo(WorkshopItem, out ulong bytesDownloaded, out ulong bytesTotal))
+                UpdateEstimate(bytesDownloaded, bytesTotal);
+            else
+                UpdateEstimate(0, 0);
+
             // Prevent Appearing for Very Fast Downloads
             yield return new WaitForSeconds(DELAY_SECONDS);
 
@@ -79,8 +116,10 @@ namespace WorkshopDownloadSpinner.Services
             SetConsoleInputEnabled(false);
 
             WaitForSeconds updateDelay = new(0.1f);
-            while (SteamGameServerUGC.GetItemDownloadInfo(WorkshopItem, out ulong bytesDownloaded, out ulong bytesTotal))
+            while (SteamGameServerUGC.GetItemDownloadInfo(WorkshopItem, out bytesDownloaded, out bytesTotal))
             {
+                UpdateEstimate(bytesDownloaded, bytesTotal);
+
                 if (Console.CursorLeft != 0 && Console.BufferWidth > 0)
                     Console.CursorLeft = 0;
 
@@ -89,6 +128,9 @@ namespace WorkshopDownloadSpinner.Services
                 Console.Write(NextSpinnerChar());
                 Console.Write(' ');
                 Console.Write(DownloadProgressBar(bytesDownloaded, bytesTotal));
+                Console.Write(' ');
+                Console.Write(DownloadEstimate());
+                Console.Write("       "); // Download Estimate changes quite a bit in size, this is to prevent "0 B/s/sssss"
                 yield return updateDelay;
             }
 
